4f7c08240c36480c632c0062499224b5be8235a0
쭌이Jjoon <82186614+Jjoon0513@users.noreply.github.com>
2026-03-29 12:41:59 +0000

    구조...

--- src/neuron.rs
+++ src/neuron.rs
@@ -1,3 +1,48 @@
 pub struct Neuron{
+    pub inweights: Vec<f32>,
+    pub inbias: f32
+}
 
+impl Neuron {
+    pub fn forward(&self, inputs: &[f32]) -> f32{
+        elu(self.inweights.iter()
+    .zip(inputs.iter())
+    .map(|(w, x)| w * x)
+    .sum::<f32>() + self.inbias)
+    }
+}
+
+pub struct Layer {
+    pub neurons: Vec<Neuron>
+}
+
+impl Layer {
+    pub fn forward(&self, input: &[f32]) -> Vec<f32> {
+        self.neurons.iter().map(|n|
+            n.forward(input)
+        ).collect()
+    }
+}
+
+pub struct Network {
+    pub layers: Vec<Layer>
+}
+
+impl Network {
+    pub fn forward(&self, input: &[f32]) -> f32 {
+        self.layers.iter().map(|l| {
+            l.forward(input)
+        })
+    }
+}
+
+
+
+
+fn elu(x: f32) -> f32{
+    if x > 0.0 {
+        x
+    } else {
+        x.exp() - 1.0
+    }
 }
